2bb07a17fc23dd872fb16e096175bade9059ab88
Aqsakkhan <aqsakkhan04@gmail.com>
2025-12-08 16:31:02 +0530

    Delete Array directory

--- Array/Easy/second.java
+++
@@ -1,9 +0,0 @@
-// 2. Find the second largest element
-import java.util.*;
-class second{
-    public static void main(String[]args){
-         int num[]={9,7,1,2,0,10};
-         Arrays.sort(num);
-         System.out.println("The second largest number is "+num[num.length-2]);
-    }
-}
